0030b8706daa6524b4d7cc60daa7d03fcb036ce2
ahd3r <fcdd07@mail.ua>
2018-10-13 16:06:08 +0300

    'progress'

--- algorithm.py
+++ algorithm.py
@@ -1,24 +1,37 @@
-# from time import sleep
+from time import sleep
 import random
 
-# def print_item_of_mas(mas):
-# 	for item in mas:
-# 		sleep(1)
-# 		print(item)
+# Try to use a call def by other def
+def tryy():
+	print('HI')
+
+def try_try():
+	tryy()
+
+# try_try()
+
+# Try sleep function
+def print_item_of_mas(mas):
+	for item in mas:
+		sleep(1)
+		print(item)
 
 # print_item_of_mas([1,2,3,4,5,6,7,8,9])
 
-# def qs(mas):
-# 	if len(mas)<=1:
-# 		return mas
-# 	else:
-# 		pivot=mas[0]
-# 		less_then_pivot=[i for i in mas[1:] if i <= pivot]
-# 		more_then_pivot=[i for i in mas[1:] if i > pivot]
-# 		return qs(less_then_pivot)+[pivot]+qs(more_then_pivot)
+# Algorithm quick sort
+def qs(mas):
+	if len(mas)<=1:
+		return mas
+	else:
+		pivot=mas[0]
+		less_then_pivot=[i for i in mas[1:] if i <= pivot]
+		more_then_pivot=[i for i in mas[1:] if i > pivot]
+		return qs(less_then_pivot)+[pivot]+qs(more_then_pivot)
 
 # print(qs([36,256,32,76,36,4326,543,4,85,3,1,534,7,653,473]))
+
 #---------------------------------------------------------------------
+
 # 1
 # The task of finding the easiest way for graph with some weight
 class Graph():
@@ -34,25 +47,33 @@ class Graph():
 		return 'Class for count the easiest way for graph with some weight!'
 
 	def create_a_node_for_graph(self):
+		node_2=[]
 		self.node.append('(1,start)')
 		for x in range(self.lengh):
 			for y in range(random.randint(1,3)): # level
-				self.node.append(f'({x + 2}.{y+1})')
+				node_2.append(f'({x + 2}.{y+1})')
+			self.node.append(node_2)
+			node_2=[]
 		self.node.append(f'({self.lengh + 2},finish)')
 		return self.node
 
 	def create_a_connections_for_graph(self):
-		for c in range(len(self.node)):
-			self.connect[f'{self.node[c]}:>{self.node[c]}'] = random.choice(self.weight)
+		node_3=self.node
+		node_3.remove(node_3[0])
+		node_3.remove(node_3[len(node_3)-1])
+		for c in self.node:
+			for cc in node_3:
+				self.connect[f'{c} > {cc}']=random.choices(self.weight)
 		return self.connect
 
 	def find_the_way(self):
 		return self.res_way, self.res_weight
 
 g=Graph(lengh=3,weight=[1,2,3,4])
-print(g.create_a_node_for_graph())
+g.create_a_node_for_graph()
+# print(g.create_a_node_for_graph())
 print(g.create_a_connections_for_graph())
-print(g.find_the_way())
+# print(g.find_the_way())
 
 # The task of finding the easiest way for graph with some weight include negative
 
